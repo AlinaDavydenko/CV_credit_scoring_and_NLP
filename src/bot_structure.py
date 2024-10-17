633de6baac961df71b28c296847dd43c540278b2
Alina Davydenko <Alinadavydenko620@gmail.com>
2024-10-17 22:34:18 +0300

    f

--- src/bot_structure.py
+++ src/bot_structure.py
@@ -25,25 +25,7 @@ async def start_command(message: types.Message):
 @dp.message()
 async def exco(message: types.Message):
     """ The function responds with the user's message """
-    text = message.text.lower()
-    for word in ['fuck', 'bitch', 'shit', 'gay']:
-        if text in word:
-            await message.answer(f'{message.text.upper()} U TOO, BRO!!!')
-            break
-    for word in ['dick', 'prick', 'cunt', 'pussy', 'ass']:
-        if text in word:
-            await message.answer(f'U {message.text.upper()} IS BIG!!! \nInnit?')
-            break
-    for word in ['hi', 'hello', 'bye', 'goodbye']:
-        if word in text:
-            await message.answer(f'{message.text.upper()} BRO \ngo to your asshole...')
-            break
-    for word in ['jokes', 'fun', 'joke', 'trick', 'prank']:
-        if text in word:
-            await message.answer(f'What do you call a fake noodle? \nAn impasta.')
-            break
-    else:
-        await message.answer(f'{message.text.upper()}!!!')
+    await message.answer(f'{message.text.upper()}!!!')
 
 
 async def main_bot() -> None:
